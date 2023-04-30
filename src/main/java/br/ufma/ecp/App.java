d4081dfa66438846b76c77dc23dbebc849e923fd
Lucas Amorim <lucasamorim.slv@gmail.com>
2023-04-30 15:28:14 -0300

    Ignorando comentários

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -14,11 +14,19 @@ public class App
     {
 
     
-        String input = "45 \"hello\" variavel + while < , if";
-        Scanner scan = new Scanner (input.getBytes());
-        for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
-        System.out.println(tk);
-    }
+        String input = """
+            // é um comentario 10
+            45 \"hello\" variavel + while < , if
+            /*
+            comentario em bloco
+            */
+            42 ola
+            
+            """;
+          Scanner scan = new Scanner (input.getBytes());
+          for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
+              System.out.println(tk);
+          }
 
         /*
         Parser p = new Parser (input.getBytes());
